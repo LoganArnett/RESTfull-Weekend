1bb9e11e989c9ea512c73df6297697216189a66f
Logan <logan@loganarnett.com>
2014-11-21 16:34:26 -0500

    add searchFinal to js

--- app/app.js
+++ app/app.js
@@ -1,1 +1,5 @@
 /* Davidbot app.js */
+
+var searchFinal = "AngularJS&qs=n&form=QBRE&pq=angularjs&sc=8-9&sp=-1&sk=&cvid=e1d0054e1f4c475f99a6db9e22af69f7";
+
+$("#bingit").attr('src', "http://www.letmebingthatforyou.com/search?q=" + searchFinal );
